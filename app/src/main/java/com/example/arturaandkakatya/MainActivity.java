dc14b1f34a322a57d170351f171f1456bcecf30a
Arturirurich <arturkawin@gmail.com>
2022-12-27 03:34:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/arturaandkakatya/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.arturaandkakatya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Cat myCat;
+    int z;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Cat cat1 = new Cat();
+        cat1.catchMouse(1);
+        cat1.catchMouse(3);
+
+    }
+}
